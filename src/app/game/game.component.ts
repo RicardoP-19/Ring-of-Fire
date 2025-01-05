ef4cb5bb18dab89dc045fb0e113ecfd57da16d98
Ricardo <ricardo-pinto@web.de>
2025-01-05 16:54:17 +0100

    add_playcards

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -1,5 +1,7 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { Game } from './../../models/game';
+import { log } from 'console';
 
 @Component({
   selector: 'app-game',
@@ -10,9 +12,31 @@ import { Component } from '@angular/core';
 })
 export class GameComponent {
   pickCardAnimation = false;
+  currentCard: string = '';
+  game?: Game;
+
+  constructor() {}
+
+  ngOnInit(): void {
+    this.newGame();
+  }
+
+
+  newGame() {
+    this.game = new Game();
+  }
 
   takeCard(){
-    this.pickCardAnimation = true;
+    if (!this.pickCardAnimation) {
+      this.currentCard = this.game?.stack.pop() ?? '';
+      this.pickCardAnimation = true;
+      console.log('new card' + this.currentCard);
+      console.log('game is', this.game);      
+    }
+    setTimeout(() => {
+      this.pickCardAnimation = false;
+      this.game?.playerCards.push(this.currentCard);
+    }, 1200);
   }
 
 }
